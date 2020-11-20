1a2297a6933e46bbf6e1a3cd8749b8a65c40ad35
Tibor Kircsi <tkircsi@gmail.com>
2020-11-20 17:23:19 +0100

    default web app

---
+++ cmd/web/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	// Initialize a new http.Server struct. We set the Addr and Handler fields so
+	// that the server uses the same network address and routes as before, and set
+	// the ErrorLog field so that the server now uses the custom errorLog logger in
+	// the event of any problems.
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s\n", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
